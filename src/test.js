73b7d1e35d507405b6db36544102bd0112502e45
shaosuo <shaosuo@youmi.net>
2019-02-28 17:10:34 +0800

    修改项目结构，测试jest

--- src/test.js
+++ src/test.js
@@ -1,3 +1,7 @@
 export const sum = (a, b) => {
   return a + b
 }
+
+export const truthiness = () => {
+  return true
+}
--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,5 +1,9 @@
-import { sum } from '../test.js'
+import { sum, truthiness } from '../test.js'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
+})
+
+test('turhiness is equal ture', () => {
+  expect(truthiness()).toBeTruthy()
 })
